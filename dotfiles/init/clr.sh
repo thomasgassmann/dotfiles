ae45a286cf226f3f701d13bd13687d9fb352862e
Thomas Gassmann <thomas.gassmann@hotmail.com>
2019-12-09 23:15:00 +0100

    add corerun build output path to path

--- dotfiles/init/clr.sh
+++ dotfiles/init/clr.sh
@@ -5,3 +5,5 @@ export CLR_OBJDUMP=/usr/bin/objdump
 export CLR_AR=/usr/bin/ar
 export CLR_LINK=/usr/bin/link
 export CLR_NM=/usr/bin/nm
+
+export PATH=$PATH:/home/thomas/git/dotnet/runtime/artifacts/bin/coreclr/Linux.x64.Debug
